bfb0bc70f07bdec08eeb52328578d95ee8885b14
AnhTuyet1992003 <21130606@st.hcmuaf.edu.vn>
2024-11-07 22:31:14 +0700

    up thu vien

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,7 @@ import Foundation
 
 import flutter_secure_storage_macos
 import path_provider_foundation
-
-import path_provider_macos
 import video_player_avfoundation
-
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
